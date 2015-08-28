930e4764f3ed29ac509e13c2e55700bcaf99555c
pricem <mark.price@lmax.com>
2015-08-28 11:26:19 +0100

    fix script path, add plot axes titles

--- src/main/shell/example_plot.cmd
+++ src/main/shell/example_plot.cmd
@@ -1,9 +1,11 @@
 
-#set log y
 set logscale x
 unset xtics
+set xlabel "Percentile"
+set ylabel "Latency nanoseconds"
+set title "Inter-thread latency breakdown"
 
-plot './xlabels.dat' with labels center offset 0, 1.5 point, \
-'./foo.enc.decoded.hgrm' using 4:1 with lines title "latency breakdown"
+plot './xlabels.dat' with labels center offset 0, 1.5 point title "", \
+'./foo.enc.decoded.hgrm' using 4:1 with lines title "latency"
 
 pause -1
