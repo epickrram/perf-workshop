46f4cf95048dd0467ba93ec9181701620cbaea30
epickrram <mark@epickrram.com>
2018-03-23 12:46:51 +0000

    external script to set thread affinity

--- src/main/shell/set_thread_affinity.py
+++ src/main/shell/set_thread_affinity.py
@@ -15,6 +15,7 @@ for line in result.split("\n"):
     if line.find("Jps") < 0 and len(line.strip()) != 0 and line.find("perf-workshop-all-0.0.1.jar") != -1:
         pid = line.split(" ")[0].strip()
         pids = subprocess.check_output(([os.environ["JAVA_HOME"] + "/bin/jstack", str(pid)]))
+        cpu_index = 1
         for thread in pids.split("\n"):
             if thread.find("nid=0x") > -1:
                 tokens = thread.strip().split(" ")
@@ -22,6 +23,11 @@ for line in result.split("\n"):
                     if token.find("nid=0x") != -1:
                         tid = token.split("=")
                         if tokens[0].find("Producer") != -1 or tokens[0].find("Accumulator") != -1:
-                            print tokens[0] + " " + str(int(str(tid[1]), 16))
+                            # do taskset
+                            cpu = sys.argv[cpu_index]
+                            affined_tid = str(int(str(tid[1]), 16))
+                            subprocess.check_output(["taskset", "-cp", cpu, affined_tid])
+                            cpu_index += 1
+                            print "Set " + tokens[0] + "(" + affined_tid + ") to run on CPU" + cpu
                     
 
